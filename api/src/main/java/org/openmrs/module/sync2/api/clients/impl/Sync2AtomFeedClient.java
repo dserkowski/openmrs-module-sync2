2100bdf85630f5c60f16b32f6cbaecea627c43df
Daniel Serkowski <dserkowski@soldevelo.com>
2017-11-13 18:44:26 +0100

    SYNCT-34: Change Sync2AtomFeedClient to more suitable for factories creating by a beanId

--- api/src/main/java/org/openmrs/module/sync2/api/clients/impl/Sync2AtomFeedClient.java
+++ api/src/main/java/org/openmrs/module/sync2/api/clients/impl/Sync2AtomFeedClient.java
@@ -8,15 +8,18 @@ import org.ict4h.atomfeed.client.repository.AllFeeds;
 import org.ict4h.atomfeed.client.repository.jdbc.AllFailedEventsJdbcImpl;
 import org.ict4h.atomfeed.client.repository.jdbc.AllMarkersJdbcImpl;
 
-import org.openmrs.api.context.Context;
 import org.openmrs.module.atomfeed.transaction.support.AtomFeedSpringTransactionManager;
 import org.openmrs.module.sync2.api.clients.AtomFeedClient;
 import org.openmrs.module.sync2.api.exceptions.Sync2Exception;
+import org.openmrs.module.sync2.api.utils.Sync2Utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component("sync.Sync2AtomFeedClient")
+@Scope("request")
 public class Sync2AtomFeedClient implements AtomFeedClient {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(Sync2AtomFeedClient.class);
@@ -28,25 +31,15 @@ public class Sync2AtomFeedClient implements AtomFeedClient {
 	private org.ict4h.atomfeed.client.service.AtomFeedClient atomFeedClient;
 	
 	public Sync2AtomFeedClient() {
-		AtomFeedSpringTransactionManager atomFeedSpringTransactionManager = getAtomFeedSpringTransactionManager();
-		HashMap<String, String> clientCookies = new HashMap<>();
 		atomFeedProperties =  new AtomFeedProperties(); // TODO: make it more generic - create getters and setters
-		
-		atomFeedClient = new org.ict4h.atomfeed.client.service.AtomFeedClient(
-			new AllFeeds(atomFeedProperties, clientCookies),
-			new AllMarkersJdbcImpl(atomFeedSpringTransactionManager),
-			new AllFailedEventsJdbcImpl(atomFeedSpringTransactionManager),
-			atomFeedProperties,
-			getAtomFeedSpringTransactionManager(),
-			uri,
-			new Sync2FeedWorker()
-		);
 	}
 	
 	@Override
 	public void process() {
 		LOGGER.info("{} started processing", getClass().getName());
 		validateConfiguration();
+		
+		org.ict4h.atomfeed.client.service.AtomFeedClient atomFeedClient = createAtomFeedClient();
 		atomFeedClient.processEvents();
 		atomFeedClient.processFailedEvents();
 	}
@@ -57,10 +50,30 @@ public class Sync2AtomFeedClient implements AtomFeedClient {
 		}
 	}
 	
+	private org.ict4h.atomfeed.client.service.AtomFeedClient createAtomFeedClient() {
+		HashMap<String, String> clientCookies = new HashMap<>();
+		AtomFeedSpringTransactionManager atomFeedSpringTransactionManager = getAtomFeedSpringTransactionManager();
+		
+		return new org.ict4h.atomfeed.client.service.AtomFeedClient(
+				new AllFeeds(atomFeedProperties, clientCookies),
+				new AllMarkersJdbcImpl(atomFeedSpringTransactionManager),
+				new AllFailedEventsJdbcImpl(atomFeedSpringTransactionManager),
+				atomFeedProperties,
+				atomFeedSpringTransactionManager,
+				uri,
+				new Sync2FeedWorker()
+		);
+	}
+	
+	
+	private AtomFeedSpringTransactionManager getAtomFeedSpringTransactionManager() {
+		return new AtomFeedSpringTransactionManager(Sync2Utils.getSpringPlatformTransactionManager());
+	}
+	
 	public URI getUri() {
 		return uri;
 	}
-	
+
 	public void setUri(URI uri) {
 		this.uri = uri;
 	}
@@ -68,14 +81,5 @@ public class Sync2AtomFeedClient implements AtomFeedClient {
 	public AtomFeedProperties getAtomFeedProperties() {
 		return atomFeedProperties;
 	}
-	
-	private AtomFeedSpringTransactionManager getAtomFeedSpringTransactionManager() {
-		return new AtomFeedSpringTransactionManager(getSpringPlatformTransactionManager());
-	}
-	
-	private PlatformTransactionManager getSpringPlatformTransactionManager() {
-		return 	Context.getRegisteredComponents(PlatformTransactionManager.class).get(0);
-	}
-	
 }
 	
